22fd84fb491513a9857e7f4f32a0a71bd572441c
RevisedJDUDE2 <JDUDEofficial134@gmail.com>
2023-12-20 14:48:10 +0800

    Configs

---
+++ Build/CMakeFiles/Auth.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Auth.exe"
+  "Auth.exe.manifest"
+  "Auth.pdb"
+  "CMakeFiles/Auth.dir/src/main.cpp.obj"
+  "CMakeFiles/Auth.dir/src/main.cpp.obj.d"
+  "libAuth.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Auth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
