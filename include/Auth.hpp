6e50b7d35a47efa4fe4417e839caf5d3594714ac
RevisedJDUDE2 <JDUDEofficial134@gmail.com>
2023-12-20 14:57:06 +0800

    Second

--- include/Auth.hpp
+++ include/Auth.hpp
@@ -1,5 +1,14 @@
 namespace AUTHORIZATION {
+  typedef struct {
+    const char* usrname;
+    const char* pw;
+    unsigned int id;
+ } AccountsVector;
   class Accounts {
-    
+    private:
+      AccountsVector pAccVect[255];
+    public:
+      bool AddAccounts(Accounts& acc, AccountsVector av);
+      bool GenerateID(Accounts& acc, AccountsVector av);
   };
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <windows.h>
+#include <Auth.hpp>
 
 int main() {
-
+  AUTHORIZATION::Accounts ClientDatabase;
+  ClientDatabase.AddAccounts(ClientDatabase, {"Hc", "299", 0});
   return 0;
 }
